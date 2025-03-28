dab506a75f2f65bb30fb4bbde347ab7230342d1a
HaimantiSamanta <haimantisamanta1997@gmail.com>
2025-03-28 11:53:06 +0530

    Update user model

---
+++ app/admin/admin-model.js
@@ -0,0 +1,19 @@
+// const mongoose = require('mongoose');
+// const moment = require('moment');
+// const Schema = mongoose.Schema;
+
+// const userSchema = new Schema({
+   
+//     email: {
+//         type: String,
+//         default: " "
+//     },
+
+//     password: {
+//         type: String,
+//         default: " "
+//     },
+ 
+// }, { timestamps: true })
+
+// module.exports = mongoose.model('usermaster', userSchema, 'usermasters');
--- app/user/user-model.js
+++ app/user/user-model.js
@@ -81,10 +81,10 @@ const userSchema = new Schema({
         type: String,
         default: " "
     },
-    password:{
-        type: String,
-        default: " "
-    },
+    // password:{
+    //     type: String,
+    //     default: " "
+    // },
     bank_name:{
         type: String,
         default: " "
@@ -109,6 +109,26 @@ const userSchema = new Schema({
         type: String,
         default: " "
     },
+    investment_info: [{
+        invest_no: { type: String, required: true },
+        invest_type: { type: String, required: true },
+        invest_date: { type: Date, required: true },
+        invest_amount: { type: Number, required: true },
+        invest_apply_date: { type: Date },
+        invest_confirm_date: { type: Date },
+        invest_duration: { type: Number }, 
+        roi_percentage: { type: Number },
+        capital_amount: { type: Number, required: true },
+        profit_amount: { type: Number, required: true },
+        tds_deduction_percentage: { type: Number, default: 0 },
+        service_charges_deduction_percentage: { type: Number, default: 0 },
+        tds_deduction_amount: { type: Number, default: 0 },
+        sc_deduction_amount: { type: Number, default: 0 },
+        profit_amount_after_tds_sc_deduction: { type: Number },
+        net_amount: { type: Number },
+        remarks: { type: String }
+    }],
+
     uploaded_pan_file:{
         type:String,
         default:" ",   
